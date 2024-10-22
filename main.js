29db1eb36fc2e39237a4480bf64054db847c6b13
Chegarrito <ojedgar108@gmail.com>
2024-10-22 12:57:28 -0700

    create the css, js and html files, and a header

--- main.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector("#container");
-for (let i = 0; i < 16; i++){
-    const div = document.createElement("div");
-    div.setAttribute("class", "grid");
-    container.appendChild(div);
-}
