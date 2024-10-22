bdb7e817698211413d1fc0691ec155f55938266b
Chegarrito <ojedgar108@gmail.com>
2024-10-22 14:28:25 -0700

    Add  16 columns

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const boxContainer = document.querySelector(".box");
 
+for (let i = 0; i < 16; i++){
+    const columns = document.createElement("div");
+    columns.setAttribute("class", "columns");
+    boxContainer.appendChild(columns);
+}
