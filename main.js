4a3e72b2488ec4d787efeac629bc72c9975ebc3f
Chegarrito <ojedgar108@gmail.com>
2024-10-22 16:24:18 -0700

    create a 16x16 grid with flexbox and javascript

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const row = document.querySelector(".row");
 const container = document.querySelector(".container");
-for (let i = 0; i < 4; i++){
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    container.appendChild(row);
+    for (let i = 0; i < 16; i++){
+        const column = document.createElement("div");
+        column.setAttribute("class", "column");
+        row.appendChild(column);
+
+    }
+}
+    /* for (let i = 0; i < 4; i++){
     const column = document.createElement("div");
     column.setAttribute("class", "column");
     row.appendChild(column);
     
-}
+} */
