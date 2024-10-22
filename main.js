265515af344643df80026d1b3b82a91936f7595f
Chegarrito <ojedgar108@gmail.com>
2024-10-22 16:34:58 -0700

    Put the grid code inside his own function to make it look cleaner

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const row = document.querySelector(".row");
-const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.setAttribute("class", "row");
-    container.appendChild(row);
-    for (let i = 0; i < 16; i++){
-        const column = document.createElement("div");
-        column.setAttribute("class", "column");
-        row.appendChild(column);
+function crateGrid(rows, columns) {
+    const row = document.querySelector(".row");
+    const container = document.querySelector(".container");
+    for (let i = 0; i < rows; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        container.appendChild(row);
+        for (let i = 0; i < columns; i++){
+            const column = document.createElement("div");
+            column.setAttribute("class", "column");
+            row.appendChild(column);
 
+        }
     }
 }
     /* for (let i = 0; i < 4; i++){
@@ -17,3 +19,6 @@ for (let i = 0; i < 16; i++) {
     row.appendChild(column);
     
 } */
+let rows = 16;
+let columns = 16;
+crateGrid(rows, columns);
