e120962a62a55e0c23b6de5432cf15d0c383c91e
Chegarrito <ojedgar108@gmail.com>
2024-11-06 14:32:57 -0800

    Add a prompt to the javascript to ask for selecting the size of the grid and a function that calculates the size of the grid

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
-function crateGrid(rows, columns) {
+function crateGrid(gridSize) {
     const row = document.querySelector(".row");
     const container = document.querySelector(".container");
-    for (let i = 0; i < rows; i++) {
+    let gridPadding = calculateGridPadding(gridSize);
+    for (let i = 0; i < gridSize; i++) {
         const row = document.createElement("div");
         row.setAttribute("class", "row");
         container.appendChild(row);
-        for (let i = 0; i < columns; i++){
+        for (let i = 0; i < gridSize; i++){
             const column = document.createElement("div");
             column.setAttribute("class", "column");
+            
+            column.style.padding = `${gridPadding}px`;
             row.appendChild(column);
 
         }
     }
 }
 
-let rows = 16;
-let columns = 16;
-crateGrid(rows, columns);
+function calculateGridPadding(gridSize){
+    let gridPadding = 960 / gridSize;
+    // because the padding goes to the right, left we need to divide the result in two
+    gridPadding = gridPadding/2;
+    // -2 pixels for the border and make it fit the container border
+    gridPadding = gridPadding - 2;
+    return gridPadding;
+}
 
+let gridSize = prompt("Enter the size of the grid", "16");
+
+if (gridSize === null) {
+    gridSize = 16;
+}
+
+
+crateGrid(gridSize);
 // mouseTarger is a node list. It looks and acts much like an array.
 const mouseTarget = document.querySelectorAll(".column");
 
